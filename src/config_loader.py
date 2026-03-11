2ecf13f4c815df29e3618a5efa29ae491e9c2183
Brayan <brayanmwangi732@gmail.com>
2026-03-11 14:57:01 +0300

    feat:full setup

---
+++ src/config_loader.py
@@ -0,0 +1,14 @@
+import yaml
+def load_config(filename="config.yaml"):
+    path = f"/home/brayan/AiAgents/MediInsights_Ai/config/{filename}"
+
+    with open(path,"r") as file:
+        config = yaml.safe_load(file)
+
+    return config
+def load_prompt(filename="prompt_config.yaml"):
+    path=f"/home/brayan/AiAgents/MediInsights_Ai/config/{filename}"
+    with open(path,"r") as file:
+        prompt_config = yaml.safe_load(file)
+    
+    return prompt_config
